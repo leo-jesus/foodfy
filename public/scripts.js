86035fd9c8c6938fff60f13cf7c143f09331e0e2
leo-jesus <llxdgb1@gmail.com>
2020-06-26 14:41:10 -0300

    correção de bugs

--- public/scripts.js
+++ public/scripts.js
@@ -1,17 +1,8 @@
-const showHide = document.querySelector(".active")
-
 const cards = document.querySelectorAll('.card')
 for (let card of cards) {
-
-    card.addEventListener("click", () => {
-        const pageId = card.getAttribute("id")
-      window.location.href = `/recipes/${pageId}`
-    })
+  
+  card.addEventListener("click", () => {
+    const pageId = card.getAttribute("id")
+    window.location.href = `/recipes/${pageId}`
+  })
 }
-
-showHide.addEventListener("click",()=>{
-  showHide.document.querySelector('.recipe-text-box').classList.add('active')
-})
-
-
-
